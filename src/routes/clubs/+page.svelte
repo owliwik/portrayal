7ee1c8897bca7b085f8373320bcc4d556a93e6ed
Owliwik Li <michael@owliwik.com>
2024-05-31 23:19:27 +0800

    Add column "tag" and a sample filtering function

--- src/routes/clubs/+page.svelte
+++ src/routes/clubs/+page.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
 	export let data: PageData;
+
+	let showType = '';
 </script>
 
 <main>
 	<h1>Our Clubs</h1>
 
 	<div>
+		<Checkbox onCheckedChange={() => (showType = showType === '项目式' ? '' : '项目式')} />
+		<p>Showing: {showType}</p>
+	</div>
+
+	<div>
 		{#if data.clubData}
 			{#each data.clubData as club}
-				<div class="my-4">
-					<h2>Club name: {club.name}</h2>
-					<p>English name: {club.english_name}</p>
-          <p>Classified as: {club.is_formal ? 'ICCU Verified Formal Club' : 'ICCU Registered Interest Group'}</p>
+				{#if !showType || (club.tags && club.tags.includes(showType))}
+					<div class="my-4">
+						<h2>Club name: {club.name}</h2>
+						<p>English name: {club.english_name}</p>
+						<p>
+							Classified as: {club.is_formal
+								? 'ICCU Verified Formal Club'
+								: 'ICCU Registered Interest Group'}
+						</p>
 
-					<p>Description: {club.club_description}</p>
-					<p>Contact: {club.wechat_contact}</p>
-				</div>
+						<p>Description: {club.club_description}</p>
+						<p>Contact: {club.wechat_contact}</p>
+					</div>
+				{/if}
 			{/each}
 		{/if}
 	</div>
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -17,6 +17,7 @@ export type Database = {
           is_formal: boolean
           name: string
           presidents: string[]
+          tags: string[] | null
           wechat_contact: string | null
         }
         Insert: {
@@ -26,6 +27,7 @@ export type Database = {
           is_formal: boolean
           name: string
           presidents?: string[]
+          tags?: string[] | null
           wechat_contact?: string | null
         }
         Update: {
@@ -35,6 +37,7 @@ export type Database = {
           is_formal?: boolean
           name?: string
           presidents?: string[]
+          tags?: string[] | null
           wechat_contact?: string | null
         }
         Relationships: []
